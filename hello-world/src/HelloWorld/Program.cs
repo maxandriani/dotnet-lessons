3d829a2327dfe3bb52d7bce3e02d1add9d993be8
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2021-03-14 10:10:59 -0300

    Starting w/ a simple Hello World app for good lucky 😍

---
+++ hello-world/src/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrase = "Hello";
+            var name = "World";
+
+            if (args.Length > 0)
+            {
+                name = string.Join(" ", args);
+            }
+
+            Console.WriteLine($"{phrase} {name}!");
+            Console.ReadKey();
+        }
+    }
+}
