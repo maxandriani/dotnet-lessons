2e2e53ded1ba489cca4d77f091bf473a84734437
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2021-03-15 10:09:18 -0300

    Operações com inteiros

---
+++ numbers/src/Numbers/IntegerMath.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    public static class IntegerMath
+    {
+        public static int Sun(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("Não dividirás por zero!");
+            }
+            return a / b;
+        }
+
+    }
+}
---
+++ numbers/src/Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Math!");
+
+            IntegerMathFlow();
+        }
+
+        static void IntegerMathFlow()
+        {
+            Console.WriteLine("Give me two operands (positive integer numbers): ");
+
+            int a = int.Parse(Console.ReadLine()),
+                b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The Sun of {a} and {b} is {IntegerMath.Sun(a, b)}.");
+            Console.WriteLine($"The Diference between {a} and {b} is {IntegerMath.Subtraction(a, b)}.");
+            Console.WriteLine($"The Multiplication of {a} and {b} is {IntegerMath.Multiplication(a, b)}.");
+            Console.WriteLine($"The Division of {a} by {b} is {IntegerMath.Division(a, b)}.");
+            Console.ReadKey();
+        }
+
+    }
+}
