30bb5ddee319ff01d5daea1f6d2b3be159af90f6
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2021-03-15 12:44:55 -0300

    Desafio do módulo de condicionais

---
+++ branches-and-loops-challange/src/BranchesAndLoopsChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BranchesAndLoopsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um código C# para encontrar a soma de todos os inteiros de 1 a 20 divisíveis por 3... ");
+
+            int sum = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"O resultado é {sum}");
+            Console.ReadKey();
+        }
+    }
+}
