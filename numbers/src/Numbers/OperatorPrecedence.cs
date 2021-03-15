042a994e0d68e0b226ecf5191f2bda91ea139129
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2021-03-15 10:26:39 -0300

    Precedência de operadores

---
+++ numbers/src/Numbers/OperatorPrecedence.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    public static class OperatorPrecedence
+    {
+        public static void MultiplyPrecedenceOverSum(int a, int b, int c)
+        {
+            int result = a + b * c;
+            Console.WriteLine($"The result of a + b * c is {result}");
+        }
+
+        public static void ControlPrecedenceFlowWithParentesis(int a, int b, int c)
+        {
+            int result = (a + b) * c;
+            Console.WriteLine($"The result of (a + b) * c is {result}");
+        }
+
+        public static void ComplexPrecendenceFlow(int a, int b, int c)
+        {
+            int result = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine($"The result of (a + b) - 6 * c + (12 * 4) / 3 + 12 is {result}");
+        }
+
+        public static void IntegerDivisionResutnsIntegerOnly(int a, int b, int c)
+        {
+            int result = (a + b) / c;
+            Console.WriteLine($"The result of an integer division (a + b) / c returns an integer result {result}");
+        }
+    }
+}
--- numbers/src/Numbers/Program.cs
+++ numbers/src/Numbers/Program.cs
@@ -9,6 +9,7 @@ namespace Numbers
             Console.WriteLine("Hello Math!");
 
             IntegerMathFlow();
+            OperatorPrecedenceFlow();
         }
 
         static void IntegerMathFlow()
@@ -22,6 +23,20 @@ namespace Numbers
             Console.WriteLine($"The Diference between {a} and {b} is {IntegerMath.Subtraction(a, b)}.");
             Console.WriteLine($"The Multiplication of {a} and {b} is {IntegerMath.Multiplication(a, b)}.");
             Console.WriteLine($"The Division of {a} by {b} is {IntegerMath.Division(a, b)}.");
+            Console.ReadKey();
+        }
+
+        static void OperatorPrecedenceFlow()
+        {
+            Console.WriteLine($"Give me tree operands (positive integer only): ");
+            int a = int.Parse(Console.ReadLine()),
+                b = int.Parse(Console.ReadLine()),
+                c = int.Parse(Console.ReadLine());
+
+            OperatorPrecedence.MultiplyPrecedenceOverSum(a, b, c);
+            OperatorPrecedence.ControlPrecedenceFlowWithParentesis(a, b, c);
+            OperatorPrecedence.ComplexPrecendenceFlow(a, b, c);
+            OperatorPrecedence.IntegerDivisionResutnsIntegerOnly(a, b, c);
             Console.ReadKey();
         }
 
