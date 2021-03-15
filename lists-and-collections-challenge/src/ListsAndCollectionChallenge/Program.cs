70ef1c0da13b47833feca9d0e2c9442d64ef7f8b
Maxmiliano Reipert Andriani <max.andriani@becomex.com.br>
2021-03-15 13:01:35 -0300

    Desafio de listas e coleções

---
+++ lists-and-collections-challenge/src/ListsAndCollectionChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsAndCollectionChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Expanda o que você compilou até o momento com números Fibonacci. Tente escrever o código para gerar os 20 primeiros números na sequência.");
+
+            var fibo = new List<int>() { 1, 1 };
+
+            while (fibo.Count < 20)
+            {
+                var last = fibo[fibo.Count - 1];
+                var penultimate = fibo[fibo.Count - 2];
+
+                fibo.Add(last + penultimate);
+            }
+
+            Console.WriteLine($"Sequência Fibonacci: {string.Join(", ", fibo)}");
+            Console.ReadKey();
+        }
+    }
+}
